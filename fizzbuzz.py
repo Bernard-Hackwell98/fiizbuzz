00f82e99b18c2805e7de0dd14701275d4ab813dc
Bernard-Hackwell98 <143324911+Bernard-Hackwell98@users.noreply.github.com>
2024-03-01 23:19:03 +0500

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+print("Fizz Buzz Game")
+
+for num in range(1,101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print("_")
